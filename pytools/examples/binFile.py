f2427c9b0953b8f398e2f2ba41a451b6fe7d3f79
cd <claudio.donofrio@nateko.lu.se>
2020-06-17 17:26:22 +0200

    rename CpBinFile to Dobj
    simplify naming of binrary file representation
    more intuitiv and aligned to the "station" module

--- pytools/examples/binFile.py
+++ pytools/examples/binFile.py
@@ -1,27 +1,25 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 """
-Created on Thu Jun 11 20:45:00 2020
-
-@author: Claudio
+Example 1:
+    Create a digital object represenation based on a PID 
+    @author: Claudio
 """
 
 
-# ----------------------------------------------
-# for testing only, set path to ICOS pytools
-import sys
-sys.path.insert(0,'C:\\Users\\Claudio\\Documents\\GitHub\\ICOS-CP\\jupyter\\pytools')
-# ----------------------------------------------
+from icoscp.cpb.dobj import Dobj
 
-from icoscp.cpb.cpbinfile import CpBinFile
+pid = 'ZHgulniA9Ie7PhfnHZo2Z9Ga'
+f = Dobj(pid)
 
-fId = 'ZHgulniA9Ie7PhfnHZo2Z9Ga'
 
-f = CpBinFile(fId)
 if f.valid:
-    data = f.getColumns()
-else:    
-    raise SystemExit('no binary data available',0)
+    data = f.get()
+else:        
+    raise SystemExit('no binary data available', 0)
     
 # print the first few rows
 print(data.head())
-print(f.citation)
+print('citation: ', f.citation)
+
