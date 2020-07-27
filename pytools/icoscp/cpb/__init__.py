128df5faa02b721612e10ca07dc27e10b99aa685
cd <claudio.donofrio@nateko.lu.se>
2020-07-27 15:29:05 +0200

    remove pytools
    a new repo is available as pylib...

--- pytools/icoscp/cpb/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-    The cpb module gives you access to a binary representatin of 
-    digital data objects hosted at the ICOS CarbonPortal.
-    Any data column (x,y axis) you can "preview" on the data portal
-    ( https://data.icos-cp.eu/portal/ ) is accessible from this module.
-    You need to know the digital object ID (URL, persistent identification)
-    or have a sqparql query providing this information.    
-"""
--- pytools/icoscp/station/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-    The Station Module is used to give easy access to 
-    station metadata, corresponding data products and
-    digital objects (data)
-"""
