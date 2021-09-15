0e51ae29e2f0054ae4da9e645b90654dae59a6eb
idastorm <ida.storm@hotmail.com>
2021-09-15 15:47:16 +0200

    optimize network first launch!
    Tool that uses a lot of code from other notebook tools:
    Bokeh multiple variables graph with variables including land cover, but also values from the original station characterization seasonal table such as anthropogenic contribution and population sensitivity.
    The variables can be selected as well as the stations (this is new). Also, prioritizing the variables to generate a combined score (weighted differently - same principles as suitability analysis). This is in progress.

---
+++ notebooks/icos_jupyter_notebooks/optimize_network/gui_optimize_network.py
@@ -0,0 +1,265 @@
+# -*- coding: utf-8 -*-
+"""
+Created 2021-08-11
+
+@author: Claudio and Ida 
+
+"""
+
+from ipywidgets import Dropdown, SelectMultiple, HBox, VBox, Button, Output, IntText, RadioButtons,IntProgress,IntSlider, GridspecLayout,FileUpload, BoundedIntText, Textarea, Checkbox, Select, IntText, Layout
+
+import stiltStations
+from IPython.core.display import display, HTML 
+from icoscp.station import station as cpstation
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import optimize_network_functions as onf
+
+import json
+
+stiltstations = stiltStations.getStilt()
+#v['country'] + ': ' + 
+all_list_2018 = sorted([((v['country'] + ': ' + v['name'] + ' ('+ k + ')'),k) for k,v in stiltstations.items() if '2018' in v['years'] if len(v['2018']['months'])>12])
+
+def disable_enable_update_button():    
+
+    if len(selected_network_sites.options)>0:
+
+        update_button.disabled = False
+       
+    else:
+        update_button.disabled = True
+
+def set_settings(s):
+  
+    selected_network_sites.options = [station for station in all_list_2018 if station[1] in s['baseNetwork']] 
+
+def change_sites_network_options(c):
+
+    current_selection = [station_tuple[1] for station_tuple in selected_network_sites.options]
+    selection = set(list(sites_network_options.value) + current_selection)     
+    selection_tuplelist = [station for station in all_list_2018 if station[1] in selection]
+    selected_network_sites.options = sorted(selection_tuplelist)
+    disable_enable_update_button()
+
+def change_selected_network_sites(c):
+    
+    selected_network_sites.options = [station_tuple for station_tuple in selected_network_sites.options if station_tuple[1] not in selected_network_sites.value]
+    
+
+def file_set_widgets(c):
+    
+    uploaded_file = file_name.value
+    
+    #check if there is content in the dictionary (uploaded file)
+    if bool(uploaded_file):
+        settings_file=uploaded_file[list(uploaded_file.keys())[0]]['content']
+        settings_json = settings_file.decode('utf8').replace("'", '"')
+        settings_dict = json.loads(settings_json)
+        set_settings(settings_dict)
+
+#clear all the output when run the tool again
+def clear_all_output():
+    output_multiple_var_graph.clear_output()
+
+#----------- start processing -----------------
+
+def update_func(button_c):
+    
+    update_button.disabled = True
+    clear_all_output()
+    sites_network=[station_tuple[1] for station_tuple in selected_network_sites.options]
+    
+    df_saved_for_normalized = onf.normalized_dataframe(sites_network)
+    
+    with output_multiple_var_graph:
+        
+        onf.variables_graph_bokeh(df_saved_for_normalized)
+        
+    update_button.disabled = False
+        
+    
+        
+    
+
+#-----------widgets definition ----------------
+    
+style_bin = {'description_width': 'initial'}
+
+heading_site_selection = Output()
+
+with heading_site_selection:
+    display(HTML('<p style="font-size:20px;font-weight:bold;">Select sites to compare</p>'))
+
+heading_sites_network_options = Output()
+
+with heading_sites_network_options:
+    display(HTML('<p style="font-size:15px;">Options</p>'))
+    
+sites_network_options= SelectMultiple(
+    options=all_list_2018,
+    rows=14,
+    style=style_bin,
+    description='',
+    disabled=False) 
+
+sites_network_options.layout.margin = '0px 0px 0px 0px'
+
+heading_selected_network_sites  = Output()
+with heading_selected_network_sites:
+    display(HTML('<p style="font-size:15px;">Current selection (click to deselect):</p>'))
+
+heading_selected_network_sites.layout.margin = '0px 0px 0px 70px'
+
+selected_network_sites = SelectMultiple(
+    options=(),
+    style=style_bin,
+    rows=14,
+    description='',
+    disabled=False) 
+
+selected_network_sites.layout.margin = '0px 0px 0px 70px'
+
+download_output_heading = Output()
+
+with download_output_heading:
+    
+    display(HTML('<p style="font-size:15px;font-weight:bold;">Download output</p>'))
+      
+download_output_option=RadioButtons(
+        options=[('No', False), ('Yes', True)],
+        description=' ',
+        disabled=False)
+
+
+header_broad_leaf_forest = Output()
+with header_broad_leaf_forest:
+    display(HTML('<p style="font-size:15px;margin-left:4em;">Broad leaf forest</p>'))
+broad_leaf_forest = Checkbox(value=True, indent=True, layout=Layout(width='100px'))
+broad_leaf_forest_int=IntText(value=0, layout=Layout(width='70px'))
+
+header_coniferous_forest = Output()
+with header_coniferous_forest:
+    display(HTML('<p style="font-size:15px;margin-left:4em;">Coniferous forest</p>'))
+coniferous_forest = Checkbox(value=True, indent=True, layout=Layout(width='100px'))
+coniferous_forest_int=IntText(value=0, layout=Layout(width='70px'))
+
+header_mixed_forest = Output()
+with header_mixed_forest:
+    display(HTML('<p style="font-size:15px;margin-left:4em;">Mixed forest</p>'))
+mixed_forest = Checkbox(value=True, indent=True, layout=Layout(width='100px'))
+mixed_forest_int=IntText(value=0, layout=Layout(width='70px'))
+
+header_ocean = Output()
+with header_ocean:
+    display(HTML('<p style="font-size:15px;margin-left:4em;">Ocean</p>'))
+ocean = Checkbox(value=True, indent=True, layout=Layout(width='100px'))
+ocean_int=IntText(value=0, layout=Layout(width='70px'))
+
+header_other = Output()
+with header_other:
+    display(HTML('<p style="font-size:15px;margin-left:4em;">Other</p>'))
+other = Checkbox(value=True, indent=True, layout=Layout(width='100px'))
+other_int=IntText(value=0, layout=Layout(width='70px'))
+
+header_grass_shrub = Output()
+with header_grass_shrub:
+    display(HTML('<p style="font-size:15px;margin-left:4em;">Grass/shrubland</p>'))
+grass_shrub = Checkbox(value=True, indent=True, layout=Layout(width='100px'))
+grass_shrub_int=IntText(value=0, layout=Layout(width='70px'))
+
+header_cropland = Output()
+with header_cropland:
+    display(HTML('<p style="font-size:15px;margin-left:4em;">Cropland</p>'))
+cropland = Checkbox(value=True, indent=True, layout=Layout(width='100px'))
+cropland_int=IntText(value=0, layout=Layout(width='70px'))
+
+header_pasture = Output()
+with header_pasture:
+    display(HTML('<p style="font-size:15px;margin-left:4em;">Pasture</p>'))
+pasture = Checkbox(value=True, indent=True, layout=Layout(width='100px'))
+pasture_int=IntText(value=0, layout=Layout(width='70px'))
+
+header_urban = Output()
+with header_urban:
+    display(HTML('<p style="font-size:15px;margin-left:4em;">Urban</p>'))
+urban = Checkbox(value=True, indent=True, layout=Layout(width='100px'))
+urban_int=IntText(value=0, layout=Layout(width='70px'))
+
+
+header_filename = Output()
+
+with header_filename:
+    display(HTML('<p style="font-size:15px;font-weight:bold;">Load settings from file (optional)</p>'))
+
+
+file_name= FileUpload(
+    accept='.json',  # Accepted file extension e.g. '.txt', '.pdf', 'image/*', 'image/*,.pdf'
+    multiple=False  # True to accept multiple files upload else False
+)
+
+#Create a Button widget to control execution:
+update_button = Button(description='Run selection',
+                       disabled=True,
+                       button_style='danger', # 'success', 'info', 'warning', 'danger' or ''
+                       tooltip='Click me',)
+
+update_button.layout.margin = '0px 0px 0px 160px' #top, right, bottom, left
+royal='#4169E1'
+update_button.style.button_color=royal
+
+box_site_options = VBox([heading_sites_network_options, sites_network_options])
+box_site_selection = VBox([heading_selected_network_sites, selected_network_sites])
+box_site_combined = HBox([box_site_options,box_site_selection])
+
+vbox_1 = VBox([header_broad_leaf_forest, HBox([broad_leaf_forest, broad_leaf_forest_int])])
+vbox_2 = VBox([header_coniferous_forest, HBox([coniferous_forest, coniferous_forest_int])])
+vbox_3 = VBox([header_mixed_forest, HBox([mixed_forest, mixed_forest_int])])
+vbox_4 = VBox([header_ocean, HBox([ocean, ocean_int])])
+vbox_5 = VBox([header_other, HBox([other, other_int])])
+vbox_6 = VBox([header_cropland, HBox([cropland, cropland_int])])
+vbox_7 = VBox([header_pasture, HBox([pasture, pasture_int])])
+vbox_8 = VBox([header_urban, HBox([urban, urban_int])])
+
+hbox_variables1 = HBox([vbox_1,vbox_2,vbox_3,vbox_4])
+hbox_variables2 = HBox([vbox_5,vbox_6,vbox_7,vbox_8])
+
+final_row = HBox([file_name, update_button])
+#Add all widgets to a VBox:
+form = VBox([heading_site_selection, box_site_combined, hbox_variables1, hbox_variables2, final_row])
+
+#Initialize form output:
+form_out = Output()
+
+output_multiple_var_graph = Output()
+
+
+#--------------------------------------------------------------------
+
+# observers:
+def observe():    
+
+    sites_network_options.observe(change_sites_network_options, 'value')
+    selected_network_sites.observe(change_selected_network_sites, 'value')
+
+    file_name.observe(file_set_widgets, 'value')
+
+    update_button.on_click(update_func)
+
+    
+# start observation
+observe()
+
+
+#--------------------------------------------------------------------
+
+#Open form object:
+with form_out:
+        
+    display(form, output_multiple_var_graph)
+
+#Display form:
+display(form_out)    
---
+++ notebooks/icos_jupyter_notebooks/optimize_network/optimize_network_functions.py
@@ -0,0 +1,251 @@
+from bokeh.io import show, output_notebook, reset_output, export_png
+from bokeh.plotting import figure as bokeh_figure
+from bokeh.plotting import figure 
+from bokeh.models import ColumnDataSource, HoverTool, Label, Legend
+reset_output()
+output_notebook()    
+import pandas as pd
+import sys
+import os
+sys.path.append('../station_characterization')
+import stc_functions as stc
+
+
+def variables_graph_bokeh(df, variables=[]):
+    #'Broad leaf forest','Coniferous forest','Mixed forest',
+    variables = ['Natural grassland','Cropland',\
+                          'Pasture','Urban', 'Ocean', 'Unknown','Sensitivity', 'Population', \
+                          'Point source contribution', 'Anthropogenic contribution']
+
+    p = bokeh_figure(plot_width=900,
+               x_range=variables,
+               plot_height=700, 
+               y_axis_label='% of max',
+               title = 'Selected station compared to reference stations',
+               tools='pan,box_zoom,wheel_zoom,undo,redo,reset,save')
+    #list_attributes=['Station', '', 'Broad leaf forest','Coniferous forest','Mixed forest','Natural grassland','Cropland','Pasture','Urban', 'Ocean', 'Unknown']
+
+    index = 1
+
+    selected_station = 'HTM150'
+    #p = figure(x_range=variables)
+    for station in df['Station']:
+
+        #get all the values for station (row in dataframe)
+        station_values=df.loc[df['Station'] == station]
+        
+        station_values_list = []
+        for variable in variables:
+            
+            station_values_list.append(station_values[variable].values[0])
+
+            index = index + 1
+
+        if station == selected_station:
+            color='black'
+            line_width = 3
+        else:
+            color='grey'
+            line_width = 1
+        p.line(variables, station_values_list, name=station, line_width=line_width, color=color, legend_label=station)
+
+    p.xaxis.major_label_orientation = "vertical"
+    #Set title attributes:
+    p.title.align = 'center'
+    p.title.text_font_size = '13pt'
+    p.title.offset = 15
+
+    #Set label font style:
+    p.xaxis.axis_label_text_font_style = 'normal'
+    p.yaxis.axis_label_text_font_style = 'normal'
+    p.xaxis.axis_label_standoff = 15 #Sets the distance of the label from the x-axis in screen units
+    p.yaxis.axis_label_standoff = 15 #Sets the distance of the label from the y-axis in screen units
+
+    p.xaxis.major_label_text_font_size = "15pt"
+    p.yaxis.major_label_text_font_size = "15pt"
+    p.xaxis.axis_label_text_font_size = "15pt"
+    p.yaxis.axis_label_text_font_size = "15pt"
+
+    #Set grid format:
+    p.grid.grid_line_alpha = 0
+    p.ygrid.band_fill_color = "olive"
+    p.ygrid.band_fill_alpha = 0.1
+
+    #Deactivate hover-tool, which is by default active:
+    p.toolbar.active_inspect = None
+
+    p.legend.location = "top_left"
+    p.legend.click_policy="hide"
+
+    show(p)
+#also in stc_functions:
+def normalized_dataframe(all_stations, variables=[]):
+
+    df_2018 = pd.DataFrame(columns=['Station', 'Broad leaf forest','Coniferous forest','Mixed forest',\
+                                    'Natural grassland','Cropland','Pasture','Urban', 'Ocean', 'Unknown',
+                                   'Sensitivity', 'Population', 'Point source contribution', 'Anthropogenic contribution'])
+
+    #change paths later
+    land_cover_2018=pd.read_csv(os.path.join('optimize_network','land_cover_2018.csv'))
+    sens_pop_pointsouce_anthro = pd.read_csv(os.path.join('optimize_network','seasonal_table_values.csv'))
+
+    index=0
+    
+    broad_leaf_forest, coniferous_forest, mixed_forest, ocean, other, grass_shrub, cropland, pasture, urban, unknown = stc.import_landcover_HILDA(year='2018')
+
+    fp_pop= stc.import_population_data(year=2018)
+    fp_point = stc.import_point_source_data()
+
+    for station in all_stations:
+
+        # get land cover data:
+        if station in set(land_cover_2018.station):
+
+            broad_leaf_forest_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'broad_leaf_forest'].iloc[0]
+            coniferous_forest_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'coniferous_forest'].iloc[0]
+            mixed_forest_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'mixed_forest'].iloc[0]
+            ocean_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'ocean'].iloc[0]
+            other_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'other'].iloc[0]
+            grass_shrub_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'grass_shrub'].iloc[0] 
+            cropland_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'cropland'].iloc[0]
+            pasture_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'pasture'].iloc[0]
+            urban_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'urban'].iloc[0] 
+            unknown_station = land_cover_2018.loc[land_cover_2018['station'] == station, 'unknown'].iloc[0]
+
+        else:
+
+            date_range = pd.date_range(start='2018-01-01', end='2019-01-01', freq='3H')
+
+            #not including midnight between dec 31 and jan 1
+            date_range = date_range[:-1]
+
+            nfp, fp_station, lon, lat, title = stc.read_aggreg_footprints(station, date_range)
+
+            if fp_station is None:
+                display(HTML('<p style="font-size:15px;">Not all footprints for ' + station + '. Use the <a href="https://stilt.icos-cp.eu/worker/" target="blank">STILT on demand calculator</a> to compute footprints for year 2018.</p>'))
+                #continue
+                break
+
+            else:
+                print('computing land cover values for ', station)
+
+                broad_leaf_forest_station = (fp_station * broad_leaf_forest).sum()
+                coniferous_forest_station = (fp_station * coniferous_forest).sum()
+                mixed_forest_station = (fp_station * mixed_forest).sum()
+                ocean_station = (fp_station * ocean).sum()
+                other_station = (fp_station * other).sum()
+                grass_shrub_station = (fp_station * grass_shrub).sum()
+                cropland_station = (fp_station * cropland).sum()
+                pasture_station = (fp_station * pasture).sum()
+                urban_station = (fp_station * urban).sum()
+                unknown_station = (fp_station * unknown).sum()
+
+        # get the data for population, point sourcer contribution, anthropogenic contribution
+        station_year = station + '_2018'
+        if station_year in set(sens_pop_pointsouce_anthro.station_year):
+            sens_station = sens_pop_pointsouce_anthro.loc[sens_pop_pointsouce_anthro['station_year'] == station_year, 'sens_whole'].iloc[0]
+            pop_station = sens_pop_pointsouce_anthro.loc[sens_pop_pointsouce_anthro['station_year'] == station_year, 'pop_whole'].iloc[0]
+            pointsource_station = sens_pop_pointsouce_anthro.loc[sens_pop_pointsouce_anthro['station_year'] == station_year, 'point_whole'].iloc[0]
+            anthro_station = sens_pop_pointsouce_anthro.loc[sens_pop_pointsouce_anthro['station_year'] == station_year, 'anthro_whole'].iloc[0]
+        else:
+
+            date_range = pd.date_range(start='2018-01-01', end='2019-01-01', freq='3H')
+
+            #not including midnight between dec 31 and jan 1
+            date_range = date_range[:-1]
+
+            nfp, fp_station, lon, lat, title = stc.read_aggreg_footprints(station, date_range)
+
+            if fp_station is None:
+                display(HTML('<p style="font-size:15px;">Not all footprints for ' + station + '. Use the <a href="https://stilt.icos-cp.eu/worker/" target="blank">STILT on demand calculator</a> to compute footprints for year 2018.</p>'))
+
+                break
+
+            else:
+                
+                print('computing values for ', station)
+
+                sens_station = fp_station.sum()
+                pop_station = (fp_station * fp_pop).sum()
+                pointsource_station = (fp_station * fp_point).sum()
+
+                #steps to get the modelled concentraion values of anthropogenic contributions
+                timeselect_list=[0, 3, 6, 9, 12, 15, 18, 21]
+                df_stilt_timeseries = stc.read_stilt_timeseries(station, date_range, timeselect_list)
+                df_stilt_timeseries_mean = df_stilt_timeseries.mean()
+
+                anthro_station = df_stilt_timeseries_mean['co2.industry']+df_stilt_timeseries_mean['co2.energy']+ \
+                df_stilt_timeseries_mean['co2.transport']+ df_stilt_timeseries_mean['co2.others']
+
+
+        df_2018.loc[index] = [station, broad_leaf_forest_station, coniferous_forest_station, \
+                                  mixed_forest_station, grass_shrub_station, cropland_station, \
+                                  pasture_station, urban_station, ocean_station, unknown_station,\
+                                  sens_station, pop_station, pointsource_station, anthro_station]
+
+        index = index + 1   
+
+
+    #DONE GETTING ALL THE DATA: 
+    #sensitivity is the first attribut (list_item[0]) in the each of the lists (one list per station)
+    min_broad_leaf_forest=min(df_2018['Broad leaf forest'])
+    range_broad_leaf_forest=max(df_2018['Broad leaf forest'])-min_broad_leaf_forest
+
+    min_coniferous_forest=min(df_2018['Coniferous forest'])
+    range_coniferous_forest=max(df_2018['Coniferous forest'])-min_coniferous_forest
+
+    min_mixed_forest=min(df_2018['Mixed forest'])
+    range_mixed_forest=max(df_2018['Mixed forest'])-min_mixed_forest
+
+    min_natural_grassland=min(df_2018['Natural grassland'])
+    range_natural_grassland=max(df_2018['Natural grassland'])-min_natural_grassland
+
+    min_cropland=min(df_2018['Cropland'])
+    range_cropland=max(df_2018['Cropland'])-min_cropland
+
+    min_pasture=min(df_2018['Pasture'])
+    range_pasture=max(df_2018['Pasture'])-min_pasture
+
+    min_urban=min(df_2018['Urban'])
+    range_urban=max(df_2018['Urban'])-min_urban
+
+    min_ocean=min(df_2018['Ocean'])
+    range_ocean=max(df_2018['Ocean'])-min_ocean
+
+    min_unknown=min(df_2018['Unknown'])
+    range_unknown=max(df_2018['Unknown'])-min_unknown
+
+
+    min_sens=min(df_2018['Sensitivity'])
+    range_sens=max(df_2018['Sensitivity'])-min_sens
+
+    min_pop=min(df_2018['Population'])
+    range_pop=max(df_2018['Population'])-min_pop
+
+    min_point=min(df_2018['Point source contribution'])
+    range_point=max(df_2018['Point source contribution'])-min_point
+
+    min_anthro=min(df_2018['Anthropogenic contribution'])
+    range_anthro=max(df_2018['Anthropogenic contribution'])-min_anthro
+
+
+    df_saved_for_normalized=df_2018.copy()
+
+    #for station in all_stations:   
+    for station in df_saved_for_normalized['Station']:
+
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Broad leaf forest', min_broad_leaf_forest, range_broad_leaf_forest)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Coniferous forest', min_coniferous_forest, range_coniferous_forest)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Mixed forest', min_mixed_forest, range_mixed_forest)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Natural grassland', min_natural_grassland, range_natural_grassland)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Cropland', min_cropland, range_cropland)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Pasture', min_pasture, range_pasture)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Urban', min_urban, range_urban)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Ocean', min_ocean, range_ocean)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Unknown', min_unknown, range_unknown)
+
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Sensitivity', min_sens, range_sens)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Population', min_pop, range_pop)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Point source contribution', min_point, range_point)
+        df_saved_for_normalized=stc.compute_normalized(df_saved_for_normalized, station, 'Anthropogenic contribution', min_anthro, range_anthro)
+    return df_saved_for_normalized
