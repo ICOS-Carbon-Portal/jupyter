61dcaa1bbe0a794532b205b12f9430e3a770ab9d
zoiszogop@gmail.com <zoiszogop@gmail.com>
2023-05-25 13:43:05 +0200

    315 summer school 2023 v_5.0
    - Remove ENTRYPOINT script from version control.

--- notebooks/summer_school_2023.sh
+++
@@ -1,5 +0,0 @@
-git clone https://git.wur.nl/peter050/hyytiala-practicals.git /home/jovyan/education/hyytiala-practicals
-cd /home/jovyan/education/hyytiala-practicals || exit
-git fetch origin && git pull
-ln -s /home/jovyan/education/hyytiala-practicals/EXERCISES -t /home/jovyan/education/Summer-school-2023/.
-ln -s /home/jovyan/education/hyytiala-practicals/MEASUREMENTS -t /home/jovyan/education/Summer-school-2023/.
