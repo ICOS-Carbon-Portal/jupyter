98d3ae4376ad5931b2d7cd8b752467732acba991
ZogopZ <zoiszogop@gmail.com>
2025-08-28 15:09:26 +0200

    docker & front-end updates, notebook additions
    - Use the current jupyter docker image as base for all notebooks.
    - Spawn different jupyterlab single user servers for different users
      using crypto.randomUUID() in the front-end.
    - Update jupyterhub_config.py & log_execution to match the logic above.
    - Add some of the pylib examples notebook content.

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -7,31 +7,39 @@ from dockerspawner import DockerSpawner
 
 c = get_config()
 
-c.JupyterHub.authenticator_class = 'dummy'
-c.DummyAuthenticator.password = 'test'
-c.DockerSpawner.network_name = 'jupyter'
-c.JupyterHub.hub_ip = '0.0.0.0'
-c.JupyterHub.hub_connect_ip = 'hub'
-c.JupyterHub.template_paths = ['/srv/jupyterhub/templates']
+c.JupyterHub.authenticator_class = "dummy"
+c.DummyAuthenticator.password = "test"
+c.DockerSpawner.network_name = "jupyter"
+c.DockerSpawner.use_internal_ip = True
+c.DockerSpawner.host_ip = "0.0.0.0"
+c.JupyterHub.hub_ip = "0.0.0.0"
+c.JupyterHub.hub_connect_ip = "hub"
+c.JupyterHub.template_paths = ["/srv/jupyterhub/templates"]
+c.DockerSpawner.read_only_volumes = {"/data": "/data"}
 
 # Clean-up
 # Shuts down all user servers on logout
 c.JupyterHub.shutdown_on_logout = True
 c.DockerSpawner.remove = True
-c.DockerSpawner.notebook_dir = '/home/jovyan'
-c.DockerSpawner.read_only_volumes = {
-    '/data' : '/data'
+c.DockerSpawner.notebook_dir = "/home/jovyan"
+
+nb_map = {
+    "explore-icos-atmobs": "/lab/tree/explore_icos_atmObs.ipynb",
+    "curve-fitting-obspack": "/lab/tree/curve_fitting_obspack.ipynb",
+    "radiocarbon": "/lab/tree/radiocarbon.ipynb",
 }
 
 
 class CustomSpawner(DockerSpawner):
     def start(self):
-        if 'explore-icos-atmobs' in self.user.name:
-            self.image = 'explore-icos-atmobs:latest'
-        elif 'curve-fitting-obspack' in self.user.name:
-            self.image = 'curve-fitting-obspack:latest'
-        else:
-            exit(1)
+        # Extract notebook name from username
+        notebook = self.user.name.split("&id=")[0]
+        if notebook not in nb_map:
+            raise ValueError(f"Unknown notebook: {notebook}")
+
+        self.image = notebook  # use the notebook's Docker image
+        self.default_url = nb_map[notebook]
+
         return super().start()
 
 
--- icosbase/log_execution.py
+++ icosbase/log_execution.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 import asyncio
 import os
+
 # Related third party imports.
 from IPython import get_ipython
 import nest_asyncio
@@ -13,20 +14,23 @@ nest_asyncio.apply()
 
 raw_cell_content = None
 
+
 async def log_event():
     matomo_url = "https://matomo.icos-cp.eu/matomo.php"
     payload = {
-        'idsite': '10',  # Matomo site ID
-        'rec': '1',  # Record the request
-        'action_name': 'cell execution',
-        'url': 'https://ganymede.icos-cp.eu',
-        'uid': os.getenv('JUPYTERHUB_USER').replace('-', '_'),
+        "idsite": "10",  # Matomo site ID
+        "rec": "1",  # Record the request
+        "action_name": "cell execution",
+        "url": "https://ganymede.icos-cp.eu",
+        "uid": os.getenv("JUPYTERHUB_USER").split("&id=")[0].replace("-", "_"),
     }
     requests.get(matomo_url, params=payload)
+
 
 def capture_raw_cell_content(info):
     global raw_cell_content
     raw_cell_content = info.raw_cell.strip()
+
 
 def log_cell_execution(info):
     global raw_cell_content
@@ -34,7 +38,8 @@ def log_cell_execution(info):
         asyncio.run(log_event())
         raw_cell_content = None
 
+
 ipython = get_ipython()
 if ipython:
-    ipython.events.register('pre_run_cell', capture_raw_cell_content)
-    ipython.events.register('post_run_cell', log_cell_execution)
+    ipython.events.register("pre_run_cell", capture_raw_cell_content)
+    ipython.events.register("post_run_cell", log_cell_execution)
