5ea4fb98a5b5b52278f00967f5d1f421e7988479
ZogopZ <zoiszogop@gmail.com>
2025-09-17 16:27:58 +0200

    environment selection & content removal
    - Remove previous notebook separation & content
    - Implement radio buttons next to notebooks instead of categories
    - Add notebooks to different categories. Notebooks under the same category
      launch the same environment (for now)
    - Add description question mark for each category
    - Rename core notebooks to ICOS notebooks

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -7,43 +7,55 @@ from dockerspawner import DockerSpawner
 
 c = get_config()
 
-c.JupyterHub.authenticator_class = "dummy"
-c.DummyAuthenticator.password = "carboncloud"
-c.DockerSpawner.network_name = "jupyter"
-c.DockerSpawner.use_internal_ip = True
+c.DockerSpawner.allowed_images = [
+    "pylib-examples:latest",
+    "icos-notebooks:latest",
+    "awesome",
+]
 c.DockerSpawner.host_ip = "0.0.0.0"
-c.JupyterHub.hub_ip = "0.0.0.0"
-c.JupyterHub.hub_connect_ip = "hub"
-c.JupyterHub.template_paths = ["/srv/jupyterhub/templates"]
-c.DockerSpawner.read_only_volumes = {"/data": "/data"}
-
-# Clean-up
-# Shuts down all user servers on logout
-c.JupyterHub.shutdown_on_logout = True
-c.DockerSpawner.remove = True
+c.DockerSpawner.network_name = "jupyter"
 c.DockerSpawner.notebook_dir = "/home/jovyan"
+c.DockerSpawner.read_only_volumes = {"/data": "/data"}
+c.DockerSpawner.remove = True
+c.DockerSpawner.use_internal_ip = True
+c.DummyAuthenticator.password = "carboncloud"
+c.JupyterHub.authenticator_class = "dummy"
+c.JupyterHub.hub_connect_ip = "hub"
+c.JupyterHub.hub_ip = "0.0.0.0"
+c.JupyterHub.shutdown_on_logout = True
+c.JupyterHub.template_paths = ["/srv/jupyterhub/templates"]
 
 
 class CustomDockerSpawner(DockerSpawner):
-    def options_form(self, spawner):
+    @staticmethod
+    def options_form(spawner):
         template_path = "/srv/jupyterhub/templates/custom_options_form.html"
         with open(template_path, "r") as f:
             return f.read()
 
-    def options_from_form(self, formdata):
-        # No defaults: require explicit choice
+    @staticmethod
+    def options_from_form(form_data):
         options = {}
-        env_choice = formdata.get("env")
-        if env_choice:
-            options["env"] = env_choice[0]
+        if form_data:
+            image, notebook = form_data.get("env")[0].split("&nb=")
+            options["image"] = image
+            if notebook in ["curve_fitting_obspack.ipynb", " radiocarbon.ipynb"]:
+                options["notebook"] = f"/lab/tree/icos-jupyter-notebooks/{notebook}"
+            else:
+                options["notebook"] = f"/lab/tree/{notebook}"
+            if image == "awesome":
+                raise ValueError(
+                    "You have been served with an awesome image! Congratulations!!!"
+                )
         return options
 
     async def start(self):
-        if "env" not in self.user_options:
+        if "image" not in self.user_options:
             raise ValueError(
                 "You must select an environment before starting the server."
             )
-        self.image = self.user_options["env"]
+        self.image = self.user_options["image"]
+        self.default_url = self.user_options["notebook"]
         return await super().start()
 
 
